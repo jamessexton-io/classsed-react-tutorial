dd4cd564f649bd577dbafd0264d8fdd0c0c1951f
James Sexton <me@jamessexton.io>
2022-09-30 14:49:02 +0100

    Publishing to github for troubleshooting support

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the body',
+            createdAt: '2022-09-30T09:54:13.808Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
---
+++ functions/index.js
@@ -0,0 +1,92 @@
+const firebase = require('firebase/app')
+const functions = require("firebase-functions");
+const admin = require("firebase-admin")
+const auth = require("firebase-auth")
+
+const app = require('express')() 
+
+admin.initializeApp()
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCTdKty519lxcxmUSTPQAbjd8ZjhCGk4vo",
+    authDomain: "classsed-react-tutorial.firebaseapp.com",
+    projectId: "classsed-react-tutorial",
+    storageBucket: "classsed-react-tutorial.appspot.com",
+    messagingSenderId: "418772928902",
+    appId: "1:418772928902:web:5a7d3939e738aa97ac0407"
+  };
+
+firebase.initializeApp(firebaseConfig)
+
+//Sign-up route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        userHandle: req.body.userHandle,
+    }
+
+    //TODO Validate Data
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        console.log("tried to create email")
+        
+        .then(data => {
+            return res.status(201).json({message: `user ${data.user.uid} signed up successfully`})
+        })
+        .catch((err) => {
+            console.log(err.error)
+            return res.status(500).json({error: err.code})
+        })
+
+        return res.status(201) 
+
+ })
+
+
+//Scream routes
+app.get('/screams', (req, res) => {
+    admin.firestore()
+    .collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(data => {
+        let screams = []
+        data.forEach(doc => {
+            screams.push({
+                screamId: doc.id,
+                body: doc.data().body,
+                userHandle: doc.data().userHandle,
+                createdAt: doc.data().createdAt
+            })
+        })
+        return res.json(screams)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.post('/scream', (req, res) => {
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: new Date().toISOString
+    }
+
+    admin.firestore()
+        .collection('screams')
+        .add(newScream)
+        .then(doc => {
+            res.json({message: `document ${doc.id} created sucessfully`})
+        })
+        .catch((err) => {
+            res.status(500).json({error: "something went wrong"})
+            console.log(err)
+        })
+
+ }) 
+
+ 
+
+ exports.api = functions.region('europe-west1').https.onRequest(app)
